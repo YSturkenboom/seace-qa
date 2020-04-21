90d60f789287add8a2e23691bca43324e8769814
Ysturkenboom <y.a.sturkenboom@gmail.com>
2020-04-21 21:57:29 +0200

    Added GMaps integration, logic for icons & filters, map centering

--- src/MapScreen.tsx
+++ src/MapScreen.tsx
@@ -1,10 +1,11 @@
+/// @ts-nocheck
 import React, { useState, useEffect } from 'react';
 import './MapScreen.css';
 import { Filter } from './components/MapFilter'
 import { MapLegend } from './components/MapLegend'
 import { MapRegionIndicator } from './components/MapRegionIndicator';
-import { CompanyLayout } from './interfaces';
-// import GoogleMapReact from 'google-map-react';
+import { CompanyLayout, LocationLayout } from './interfaces';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 const { GoogleSpreadsheet } = require('google-spreadsheet');
  
@@ -18,8 +19,10 @@ function MapScreen() {
   const [region, setRegion] = useState<string>('uk');
   const [companies, setCompanies] = useState<Array<CompanyLayout> | null>(null)
   const [loading, setLoading] = useState<boolean>(false);
-  const [searchLocation, setSearchLocation] = useState<{lat: number, lon: number} | null>({lat: 50.8742724, lon: 0.6619475})
+  const [centeredOn, setCenteredOn] = useState<LocationLayout | null>({lat: 50.8742724, lon: 0.6619475})
+  const [searchLocation, setSearchLocation] = useState<LocationLayout | null>({lat: 50.8742724, lon: 0.6619475})
 
+  const mapRef = React.createRef();
   const fetchGoogleSheet = async () => {
     await doc.useServiceAccountAuth(require('./SEACE Installer Locator-5a80e6b026be.json'));
   }
@@ -54,25 +57,54 @@ function MapScreen() {
     fetchCompaniesForRegion(region);
   }, [region]);
 
+
+  const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{ lat: 51.8266266, lng: -2.2714084 }}
+      ref={mapRef}
+    >
+      {/* <Marker position={{ lat: 51.8266266, lng: -2.2714084 }} /> */}
+      {companies && companies.map(c => {
+        const productType = String(c['Product Type']).toLowerCase();
+        // const icon = productType === 'cooling' ? require('./cool.png') : require('./heat.png')
+        return <Marker 
+          position={{ lat: parseFloat(String(c['Latitude'])), lng: parseFloat(String(c['Longitude'])) }} 
+          onClick={() =>{ mapRef.current.panTo({ lat: parseFloat(String(c['Latitude'])), lng: parseFloat(String(c['Longitude'])) });}}// icon={{url: icon}}
+        />
+      })}
+      {/* <Marker position={{ lat: 51.8266266, lng: 150.644 }} /> */}
+    </GoogleMap>
+  ))
+
   return (
-    <div className="map-screen">
-      {/* <iframe
+    /*<div className="map-screen">*/
+      /* <iframe
         width="100%"
         height="100%"
         style={{border:0}}
         src="https://www.google.com/maps/embed/v1/place?key=API_KEY
           &q=Space+Needle,Seattle+WA">
-      </iframe> */}
-      {/* <GoogleMapReact
+      </iframe> */
+      /* <GoogleMapReact
         bootstrapURLKeys={{ key: 'gme-samsungsds' }}
         defaultCenter={{lat: 59.95, lng: 30.33}}
         defaultZoom={11}
-      > */}
+      > */
+      <div>
+        <MyMapComponent
+          className="map-screen"
+          isMarkerShown={true}
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100vh` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        >
+        </MyMapComponent>
         <Filter onSubmit={() => {}} loading={loading} companies={companies} searchLocation={searchLocation}/>
         <MapLegend isOpened={true}/>
         <MapRegionIndicator region={region}/>
-      {/* </GoogleMapReact> */}
-    </div>
+      </div>
   );
 }
 
